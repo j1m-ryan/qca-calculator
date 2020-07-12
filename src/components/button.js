16f8de645778aa9638465466c8e9c000bd49179b
J1M-RYAN <jimmih15@gmail.com>
2020-07-12 14:42:52 +0100

    orange clr button

--- src/components/button.js
+++ src/components/button.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "../index.css";
-const Button = ({ grade, onClick }) => (
-  <button onClick={onClick} className="btn">
-    {grade}
-  </button>
-);
+const Button = ({ grade, onClick }) => {
+  const classname = grade === "CLR" ? "btn clr" : "btn";
+
+  return (
+    <button onClick={onClick} className={classname}>
+      {grade}
+    </button>
+  );
+};
 
 export default Button;
