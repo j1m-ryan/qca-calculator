11c28fab794b790b8fa1c5f151933f6ce2883582
J1M-RYAN <jimmih15@gmail.com>
2020-08-15 20:26:08 +0100

    Updates

---
+++ precache-manifest.e8f9ef3ff169ff3bd31f7a3578ece490.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e9bef4c27ad18b6874fd052b48d2158",
+    "url": "/qca-calulator/index.html"
+  },
+  {
+    "revision": "2705a85b8e4e72e4b2db",
+    "url": "/qca-calulator/static/css/main.963f1a01.chunk.css"
+  },
+  {
+    "revision": "7e9816f882e6f610bc7c",
+    "url": "/qca-calulator/static/js/2.2fccf3a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/qca-calulator/static/js/2.2fccf3a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2705a85b8e4e72e4b2db",
+    "url": "/qca-calulator/static/js/main.6f7c199b.chunk.js"
+  },
+  {
+    "revision": "24abcbcf777ad00f3945",
+    "url": "/qca-calulator/static/js/runtime-main.61bf56dc.js"
+  }
+]);
