043d901f42310fabeaac2d3b9f0d6c1fc1da6615
Jim Ryan <mail@jimryan.ie>
2024-02-16 18:20:08 +0000

    build

---
+++ build/precache-manifest.372c05f946db9966f793874fc2be8554.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d30a33f7a19cbfe3a9048379e8e1fd7d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "63d5223ed29dd0eca6fc",
+    "url": "./static/css/main.0850a994.chunk.css"
+  },
+  {
+    "revision": "7e9816f882e6f610bc7c",
+    "url": "./static/js/2.2fccf3a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2fccf3a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63d5223ed29dd0eca6fc",
+    "url": "./static/js/main.c7e417ac.chunk.js"
+  },
+  {
+    "revision": "d37bb75cd4adbd8c8807",
+    "url": "./static/js/runtime-main.497b767f.js"
+  }
+]);
